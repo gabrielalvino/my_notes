b6cd9b2cb4e423c1f8dcebba71f4198b98faa869
Gabriel Alvino <98842701+gabrielalvino@users.noreply.github.com>
2022-06-21 19:29:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// var numero = 10;
+
+// for (var i = 3; i = numero; i++){
+//     console.log("Execultado o for, pela  " + i + " vez")
+
+// }
+
+//       console.log("Acabou");
+// var numero = 5;
+
+// for (var i = 0, i < numero; i++){
+
+//   console.log("Execultado o for, pela " + i + " vez");
+
+
+// }
+
+// console.log("Acabou");
+
+
+
+
+//  While
+
+var numero = Math.random() * 100;
+
+while (numero < 90){
+    console.log(numero);
+    numero = Math.random()  100;
+}
+
+console.log(numero);
+console.log("acabou");
